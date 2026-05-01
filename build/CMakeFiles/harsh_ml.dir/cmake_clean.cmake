0607a0b3aa0aa66f4e6d0e90206fb6286d7863da
Harsh . <harsh.@Harshs-MacBook-Air.local>
2026-05-02 00:34:32 +1000

    facing issue with type conversion of value

---
+++ build/CMakeFiles/harsh_ml.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/harsh_ml.dir/src/main.cpp.o"
+  "CMakeFiles/harsh_ml.dir/src/main.cpp.o.d"
+  "harsh_ml"
+  "harsh_ml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/harsh_ml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
