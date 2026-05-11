d747641bbc4b149785de48a96e93873aa5403ea8
Harsh . <harsh.@Harshs-MacBook-Air.local>
2026-05-11 18:32:58 +1000

    reading data and structure defined

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <array>
 #include <variant>
 #include <typeinfo>
+#include <random> // Essential header
 
 
 
@@ -16,59 +17,57 @@ using double_str = std::variant<std::string , double>;
 
 struct DataContainer{
 
-    DataContainer(const std::string& filename){
+    // store data in std::vector<std::vector<double>> 
+    // { {1.0, 2.0, 3.0}, {4.0, 5.0, 6.0}, ... }
+
+    DataContainer(const std::string& filename,int headerlength = 0){
         std::ifstream file(filename);
         std::string line;
         int colNumb {0};
+        std::vector<double_str> row;
 
         // Check if the file is open
         if (!file.is_open()) {
             std::cerr << "Could not open the file!" << std::endl;
             return ;
         }
-        int headerEle{0};
         while (std::getline(file, line)) {
             std::stringstream ss(line);
             std::string value;
-            std::vector<double_str> row;
             
+            std::vector<double_str> temp; 
             // Split the line by commas
             while (std::getline(ss, value, ',')) {
-                if(headerEle == 0){
+                if(colNumb+1 < headerlength){
                     this->headerValues[colNumb++] = value;
-                }else{
+                }
+                // not able to push to row for some reason ** fix this later
+                else{
                     if(isNumber(value))
-                        row.push_back(std::stod(value));
+                        temp.push_back(std::stod(value));
                     else
-                        row.push_back(value);
+                        temp.push_back(value);
+                    
+                    if(temp.size() == headerlength){
+                        row.push_back(temp);
+                        temp.clear();
+                    }
                 }
                 
             }
-            if(headerEle == 0){
-                for(const auto& headerfield : headerValues){
-                    if(headerfield !="") this->headerLength++;
-                }
+        }
+        // this is just to test if we are getting the data correctly ** remove this later
+        for(auto& field : row){
+            for(auto& fieldTwo : field){
+                std::visit([](auto&& arg){
+                    using T = std::decay_t<decltype(arg)>;
+                    if constexpr (std::is_same_v<T, std::string>) {
+                        std::cout << fieldTwo << '\n';
+                    } else if constexpr (std::is_same_v<T, double>) {
+                        std::cout << fieldTwo << '\n';
+                    }
+                }, fieldTwo);
             }
-            headerEle = 1;
-            
-            if(row.size() !=0){
-                // const auto& field : row
-                // for (int i = 0;  i < row.size() ; i ++) {
-                    // std::cout << row[i] << " | ";
-                // }
-
-                // int main() {
-                //     std::variant<int, std::string> v = "Hello std::visit";
-
-                //     std::visit(overloaded {
-                //         [](int i) { std::cout << "Integer: " << i << std::endl; },
-                //         [](const std::string& s) { std::cout << "String: " << s << std::endl; }
-                //     }, v);
-                // }
-                std::cout << static_cast<double>(row[10]) << '\n';
-            }
-            
-
         }
         
     }
@@ -81,8 +80,7 @@ struct DataContainer{
 
     private:
         // fasted option to access . All in stack
-        std::array<std::string,15> headerValues{"", "", "", "", "", "", "", "", "", "","","","","",""};
-        int headerLength {0};
+        std::array<std::string,15> headerValues{"", "", "", "", "", "", "", "", "", "","","","","",""}; 
         // using dataValue = std::variant<std::string , double>;
         using headerFields = std::vector<
             std::vector<
@@ -90,6 +88,9 @@ struct DataContainer{
             >
         >;
         headerFields headerFieldValues;
+
+        std::vector<std::vector<double>> dataValues;
+        // we store all the data values in this
 
         bool isNumber(const std::string& s) {
             try {
@@ -99,6 +100,8 @@ struct DataContainer{
                 return false;
             }
         }
+
+
 
 
 };
@@ -130,12 +133,58 @@ struct MlModel {
 
 struct LinerRegression:MlModel{
 
+    private:
+        // model parameters
+        double alpha {0.01}; // learning rate
+        int iterations {1000};
+        std::vector<double> weights;
+        double bias {0.0};
+
+        void getInitialWeights(const int& featureNumb ){
+            // get initial weights
+            for(int i = 0; i < featureNumb; i++){
+                this->weights.push_back(0.0);
+            }
+        }
+
+        int shuffler {0};
+
+        bool checkRandomSeed(int& index , int& seed , int& dataSize , int& percentage , bool isTrain = true){
+                // we use random seed formula to shuffle data and get random samples for training and testing
+            return isTrain == ((index * shuffler + seed ) % dataSize < percentage);
+        }
+
+    public:
+
+        LinerRegression(std::vector<double>){
+            this->getInitialWeights(22);
+            // get data size
+            // add 55 + data_size to the shuffler number
+        }
+
+        void Fit(){
+            // train model
+            // y = w1*x1 + w2*x2 + ... + wn*xn + b
+            
+
+        }
+
+        void Predict(){
+            // predict model
+        }
+
+         void predictError(){
+            // MSE or something 
+        }
+        ~LinerRegression(){
+            // destructor
+        }
 };
 
 
 
 int main() {
 
-    DataContainer dataContainer{"../data/regressionData/house.csv"};
+    DataContainer dataContainer{"../data/regressionData/house.csv" , 8};
     return 0;
 }
